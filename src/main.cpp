c47e36ff6afa8745c45d108504bdd6878dfb28d8
IDsuroh <roh7891backup@gmail.com>
2025-10-03 17:55:51 +0900

    Refactor loop indices to use size_t type

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,31 @@
 // Print Structures Tester Function
 static void	printConfig(const std::vector<Server>& servers)	{
 
-	for (std::vector<Server>::size_type s = 0; s < servers.size(); ++s)	{
+	for (size_t s = 0; s < servers.size(); ++s)	{
 		const Server&	srv = servers[s];
 		std::cout << "Server #" << s << ":\n";
 
 
 		std::cout << "  listen:\n";
-		for (std::vector<std::string>::size_type i = 0; i <srv.listen.size(); ++i)
+		for (size_t i = 0; i <srv.listen.size(); ++i)
 			std::cout << "     - " << srv.listen[i] << std::endl;
 
 
 		std::cout << "	server_name:\n";
-		for (std::map<std::string, std::string>::const_iterator	it = srv.directives.begin();
+		for (size_t i = 0; i < srv.server_name.size(); ++i)
+			std::cout << "     - " << srv.server_name[i] << std::endl;
+
+
+		std::cout << "	directives:\n";
+		for (std::map<std::string, std::string>::const_iterator it = srv.directives.begin();
 			it != srv.directives.end(); ++it)
-				std::cout << "	" << it->first << " -> " << it->second << std::endl;
+			std::cout << "		" << it->first << " -> " << it->second << std::endl;
+
+		
+		std::cout << "	error_pages:\n";
+		for (std::map<std::string, std::string>::const_iterator it = srv.error_pages.begin();
+			it != srv.error_pages.end(); ++it)
+			std::cout << "		" << it->first << " -> " << it->second << std::endl;
 
 
 		std::cout << "	locations:\n";
