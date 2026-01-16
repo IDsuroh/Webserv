31f6e003ff1a46b03ba4b5680480abea9ad26952
suroh <roh7891backup@gmail.com>
2026-01-16 17:59:48 +0000

    finished making static website

--- www/cgi-bin/hello.py
+++ www/cgi-bin/hello.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 import os
+import html
 import urllib.parse
 
 qs = os.environ.get("QUERY_STRING", "")
 params = urllib.parse.parse_qs(qs)
-name = params.get("name", ["World"])[0]
+name = params.get("name", ["Nobody"])[0]
+name = html.escape(name)  # prevent HTML injection
 
 print("Status: 200 OK")
 print("Content-Type: text/html; charset=utf-8")
 print()
+
 print(f"""<!doctype html>
-<html>
-<head><meta charset="utf-8"><title>CGI Hello</title></head>
-<body style="font-family: sans-serif;">
-  <h1>Hello, {name}!</h1>
-  <p>This is a CGI response (dynamic).</p>
-  <p><a href="/demo/cgi.html">Back</a></p>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>CGI Hello</title>
+  <link rel="stylesheet" href="/static/style.css">
+</head>
+
+<body class="demo-page">
+  <main class="container demo-wrap">
+    <h1 class="demo-title">Hello, {name}!</h1>
+    <p class="demo-text">This is a CGI response (dynamic).</p>
+
+    <div class="form-actions form-actions-gap">
+      <a class="button button-lg" href="/demo/cgi.html">Back</a>
+      <a class="button button-lg" href="/">Home</a>
+    </div>
+  </main>
 </body>
 </html>""")
